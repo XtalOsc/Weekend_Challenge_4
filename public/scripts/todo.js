64fdabf557b62bbb52dc1c85d16ed223bdc11453
Xtal Osc <xtalosc@gmx.com>
2016-09-18 10:30:48 -0500

    add task to database and send list to client

--- public/scripts/todo.js
+++ public/scripts/todo.js
@@ -2,6 +2,44 @@ console.log('index.js sourced');
 
 $(document).ready(function(){
   console.log('document ready sourced');
+var taskArray = [];
+
+//ajax calls go in doc ready
 
 
-});
+//store task in database
+$('#createTask').on('click', function(){
+console.log('in createTask on click');
+
+//get user input
+// create object for tasks
+var objectToSend = {
+task : $('#taskIn').val(),
+completed : false
+}; // end object
+console.log('tasks: ',objectToSend);
+
+$.ajax({
+  type: 'POST',
+  url: '/addTask',
+  data: objectToSend,
+  success: function (data){
+    console.log('in ajax success addTask, returned ',data);
+    //push to task array
+    taskArray=data;
+    console.log('task array: ',taskArray);
+    listTasks(taskArray);
+  }//end success
+});//end ajax
+});//end createTask on click
+
+//refresh frontend to show tasks after one is added
+//complete or delete option
+//css change upon completion
+//task complete or not complete stored in database
+//delete task removed from front end and database
+
+
+});//end document ready
+
+//functions are outside of doc ready
--- server/app.js
+++ server/app.js
@@ -1,13 +1,68 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var urlEncodedParser = bodyParser.urlencoded({extended: false});
+// var urlEncodedParser = bodyParser.urlencoded({extended: false});
 var path = require('path');
 var pg = require('pg');
-var connectionString = 'postgres://localhost:5432/toDo';
-
+var connectionString = 'postgres://localhost:5432/toDoList';
+//access public folder
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
+//spin up server - listening on port 8080
 app.listen('8080', function(){
   console.log('listening on port 8080');
 });
+
+
+
+//add task to database
+app.post('/addTask', function(req, res){
+  console.log('in addTask');
+  pg.connect(connectionString, function(err,client,done){
+    if(err){
+      console.log('error: ',err);
+    }//end if
+    else{
+      console.log('connected to database for addTask');
+      var resultArray=[];
+      console.log('task in server: ', req.body);
+      var resultQuery = client.query('INSERT INTO todo (task, completed) VALUES ($1, $2) RETURNING *;', [req.body.task,req.body.completed]);
+      resultQuery.on('end', function(){
+        done();
+        return res.redirect('/List');
+      });
+    }//end else
+  });//end pg connect
+});//end addTask
+
+//get entire list from server
+app.get('/List', function(req,res){
+console.log('in List');
+  //pg connect
+  pg.connect( connectionString, function( err, client, done ){
+    //if err
+    if( err ){
+      console.log( 'error: ',err );
+    } //end if error connecting
+    else{
+      console.log( 'connected to database' );
+      // array to hold our results
+      var resultArray=[];
+      // query call to database table
+      var queryResults = client.query( 'SELECT * FROM todo' );
+      queryResults.on( 'row', function( row ){
+        // runs for each row in the query result
+        resultArray.push( row );
+      }); // end on row
+      queryResults.on( 'end', function(){
+        // we're done
+        done();
+        console.log('resultArray: ',resultArray);
+        // return result as a json version of array
+        return res.json( resultArray );
+      });//end on end
+    } // end no error
+  }); // end connect
+});//end get list
